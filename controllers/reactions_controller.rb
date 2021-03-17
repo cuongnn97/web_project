aaade5e0c248554fafa489c41ae89003a56b0b71
Cuong Nguyen <nncuong1101@gmail.com>
2021-03-17 18:00:13 +0700

    update

--- controllers/reactions_controller.rb
+++ controllers/reactions_controller.rb
@@ -8,8 +8,8 @@ class ReactionsController
     array = params.split("/")
     post_id = array[2][3..-1]
     user_id = array[3][5..-1]
-    #Reaction.create(user_id: user_id, post_id: post_id)
-    #[302, {'Location' =>"http://localhost:8080/"}, []]
+    Reaction.create(user_id: user_id, post_id: post_id)
+    [302, {'Location' =>"http://localhost:8080/"}, []]
   end
 
   def self.delete(params)
